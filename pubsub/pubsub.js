fe00a804d9bf9780350e0120a57d36f43f02541c
zlatan <zlatan.pham@gmail.com>
2019-03-16 22:01:00 +0700

    prettier

--- pubsub/pubsub.js
+++ pubsub/pubsub.js
@@ -2,23 +2,23 @@ var events = (function() {
   var topics = [];
   var hOP = topics.hasOwnProperty;
 
-  return { 
+  return {
     subscribe: function(topic, listener) {
-      if(!hOP.call(topics, topic)) topics[topic] = [];
+      if (!hOP.call(topics, topic)) topics[topic] = [];
       var index = topics[topic].push(listener) - 1;
 
-      return function(){
-        delete topics[topic][index]
-      }
+      return function() {
+        delete topics[topic][index];
+      };
     },
     publish: function(topic, info) {
-      if(!hOP.call(topics, topic)) return;
+      if (!hOP.call(topics, topic)) return;
 
       topics[topic].forEach(function(item) {
         item(info || {});
-      })
-    }
-  }
-})()
+      });
+    },
+  };
+})();
 
-module.exports = events
+module.exports = events;
