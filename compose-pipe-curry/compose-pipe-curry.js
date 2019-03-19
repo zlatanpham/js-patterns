dbf053e6b0e645980e20eb7a3fc2e4dce4a49fe5
zlatan <zlatan.pham@gmail.com>
2019-03-19 22:56:40 +0700

    c-p-c: add pipe func

--- compose-pipe-curry/compose-pipe-curry.js
+++ compose-pipe-curry/compose-pipe-curry.js
@@ -2,6 +2,11 @@ function compose (...fns) {
   return fns.reduce((a, c) => (...args) => a(c(...args)))
 }
 
+function pipe (...fns) {
+  return fns.reverse().reduce((a, c) => (...args) => a(c(...args)))
+}
+
 module.exports = {
-  compose
+  compose,
+  pipe
 }
--- compose-pipe-curry/compose-pipe-curry.test.js
+++ compose-pipe-curry/compose-pipe-curry.test.js
@@ -1,15 +1,43 @@
-const { compose } = require('./compose-pipe-curry')
+const { compose, pipe } = require('./compose-pipe-curry')
 
 function add3 (num) {
   return num + 3
 }
 
+function multiply2 (num) {
+  return num * 2
+}
+
+function minus4 (num) {
+  return num - 4
+}
+
 test('test compose func', () => {
   expect(
     compose(
-      add3,
+      minus4,
       add3,
       add3
     )(0)
-  ).toBe(9)
+  ).toBe(2)
+})
+
+test('test pipe func', () => {
+  expect(
+    pipe(
+      add3,
+      multiply2,
+      minus4
+    )(2)
+  ).toBe(6)
+})
+
+test('test compose >< pipe func', () => {
+  expect(
+    compose(
+      add3,
+      multiply2,
+      minus4
+    )(2)
+  ).toBe(-1)
 })
